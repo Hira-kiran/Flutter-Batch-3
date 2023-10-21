66e38a25fd747b31061fb1e00de1cbf5b853b0e0
Hira-kiran <hirakiran224@gmail.comgit config --global user.email hirakiran224@gmail.com>
2023-10-21 20:24:22 +0500

    add validate, add loading & commads

--- firebase_project/lib/signup_screen.dart
+++ firebase_project/lib/signup_screen.dart
@@ -14,6 +14,8 @@ class _SignupScreenState extends State<SignupScreen> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
   final FirebaseAuth auth = FirebaseAuth.instance;
+  bool loading = false;
+  final _formkey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,38 +26,68 @@ class _SignupScreenState extends State<SignupScreen> {
         padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
-            TextFormField(
-              controller: emailController,
-              decoration: const InputDecoration(hintText: "Enter Email"),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              controller: passwordController,
-              decoration: const InputDecoration(hintText: "Enter Password"),
+            Form(
+              key: _formkey,
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller: emailController,
+                    decoration: const InputDecoration(hintText: "Enter Email"),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Please enter email";
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    controller: passwordController,
+                    decoration:
+                        const InputDecoration(hintText: "Enter Password"),
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Please enter Password";
+                      }
+                      return null;
+                    },
+                  ),
+                ],
+              ),
             ),
             const SizedBox(
               height: 20,
             ),
             ElevatedButton(
                 onPressed: () {
-                  auth
-                      .createUserWithEmailAndPassword(
-                          email: emailController.text.toString(),
-                          password: passwordController.text.toString())
-                      .then((value) {
-                    toastMsg("Signup Successfully");
+                  if (_formkey.currentState!.validate()) {
+                    setState(() {
+                      loading = true;
+                    });
+                    auth
+                        .createUserWithEmailAndPassword(
+                            email: emailController.text.toString(),
+                            password: passwordController.text.toString())
+                        .then((value) {
+                      toastMsg("Signup Successfully");
 
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const LoginScreen()));
-                  }).onError((error, stackTrace) {
-                    toastMsg(error.toString());
-                  });
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const LoginScreen()));
+                    }).onError((error, stackTrace) {
+                      setState(() {
+                        loading = false;
+                      });
+                      toastMsg(error.toString());
+                    });
+                  }
                 },
-                child: const Text("Signup"))
+                child: loading == true
+                    ? const CircularProgressIndicator()
+                    : const Text("Signup"))
           ],
         ),
       ),
@@ -86,3 +118,8 @@ LEC 15:
 LEC 16:
 •	Login form with firebase
  */
+
+// LEC 17:
+// •	Add Validation
+// •	Loading
+// •	Commands (flutter clean, flutter pub get)
